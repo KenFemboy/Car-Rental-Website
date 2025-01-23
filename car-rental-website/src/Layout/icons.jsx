61a472f60374fcedaf7b44c72d5af09ea50d4299
Ken <charlesplays15@gmail.com>
2025-01-23 20:01:01 +0800

    Updates

--- car-rental-website/src/Layout/icons.jsx
+++
@@ -1,9 +0,0 @@
-import bmw from './icons/bmw.png'
-import toyota from './icons/toyota.png'
-import honda from './icons/honda.png'
-import mercedes from './icons/mercedes.png'
-import mitsubishi from './icons/mitsubishi.png'
-
-
-
-export default { bmw, toyota, honda, mercedes, mitsubishi }
