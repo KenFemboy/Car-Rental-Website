b016a522c08fa543ab199cb1b46929d027f2bcfb
Ken <charlesplays15@gmail.com>
2025-01-26 20:35:38 +0800

    ooppsie sa Error Page

--- car-rental-website/src/Layout/ErrorPage.jsx
+++ car-rental-website/src/Layout/ErrorPage.jsx
@@ -1,10 +1,11 @@
+import { Link } from 'react-router-dom'
 import './ErrorPage.css'
 function ErrorPage() {
     return (
         <>
             <div className='errorpage'>
                 <h1>Ooops an Error Occured</h1>
-                <a href='/'>Click here to go back</a>
+                <Link to="Car-Rental-Website/"><a>Click here to go back</a></Link>
             </div>
         </>
     )
