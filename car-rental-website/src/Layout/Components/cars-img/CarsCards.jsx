4029f59f900f7ce22cdce3f6cd8ede03f65d3904
Ken <charlesplays15@gmail.com>
2025-01-29 00:32:40 +0800

    goods nani for now

--- car-rental-website/src/Layout/Components/cars-img/CarsCards.jsx
+++ car-rental-website/src/Layout/Components/cars-img/CarsCards.jsx
@@ -3,7 +3,23 @@ function CarsCards() {
     return (
         <div className="carscards">
             <img src="https://placehold.co/100"></img>
-            <h1>Subaru</h1>
+            <h1>- Subaru  -</h1>
+
+            <p>Starting at /Day</p>
+            <div className='carInfo'>
+                <div><img src='https://placehold.co/30' /><p>SUV</p></div>
+                <div><img src='https://placehold.co/30' /><p>4 Doors</p></div>
+                <div><img src='https://placehold.co/30' /><p>5 Seats</p></div>
+                <div><img src='https://placehold.co/30' /><p>Gas/Diesel</p></div>
+
+            </div>
+
+            <div>
+                <button>Details</button>
+                <button>Book</button>
+            </div>
+
+
         </div>
     )
 }
