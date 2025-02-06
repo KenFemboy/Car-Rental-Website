f268fa2871038216a73d5bc12dfa548b5abfc281
Ken <charlesplays15@gmail.com>
2025-02-06 16:00:32 +0800

    usab kay libog

--- car-rental-website/src/App.jsx
+++ car-rental-website/src/App.jsx
@@ -1,8 +1,4 @@
-import Header from "./Layout/Components/Header"
-import Footer from "./Layout/Components/Footer"
-import Main from "./Layout/Components/Main"
-import ErrorPage from "./Layout/ErrorPage"
-import Rentals from "./Layout/Components/Rentals"
+
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 
 function App() {
@@ -14,17 +10,10 @@ function App() {
       <div>
 
         <Router>
-          <Header />
-
           <Routes>
-            <Route path="Car-Rental-Website/" element={<Main />} />
-            <Route path="Car-Rental-Website/Rentals/" element={<Rentals />} />
-            <Route path="*" element={<ErrorPage />} />
+
           </Routes>
         </Router>
-
-
-        <Footer />
 
       </div>
 
